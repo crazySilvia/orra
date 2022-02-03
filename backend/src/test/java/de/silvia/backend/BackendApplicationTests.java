1e5d53b569679b636cb491bff631e4dc326bd062
Silvia <silviagrosche@gmx.de>
2022-02-03 12:03:40 +0100

    + Fehler bzgl. secret in Tests behoben

--- backend/src/test/java/de/silvia/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/silvia/backend/BackendApplicationTests.java
@@ -1,9 +1,12 @@
 package de.silvia.backend;
 
-
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
+    @Test
+    void contextLoads(){
 
+    }
 }
