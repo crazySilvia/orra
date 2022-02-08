414630977d1e6e653337037e4ab817874345fc2f
Silvia <silviagrosche@gmx.de>
2022-02-08 13:26:31 +0100

    + Routing umgeschrieben + FE Design grob wieder angepasst

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -1,13 +1,22 @@
 import './NavBar.css';
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export default function NavBar() {
     return (
         <div className="navbar">
-            <a href={"/"}>Start</a>
-            <a href={"/vorrat"}>Vorrat</a>
-            <a href={"/login"}>Login</a>
-            <a href={"/register"}>Registrieren</a>
+            <Link to={"/"}>
+                <input className="button" value={"Start"} />
+            </Link>
+            <Link to={"/vorrat"}>
+                <input className="button" value={"Vorrat"} />
+            </Link>
+            <Link to={"/login"}>
+                <input className="button" value={"Login"} />
+            </Link>
+            <Link to={"/register"}>
+                <input className="button" value={"Registrieren"} />
+            </Link>
         </div>
     )
 }
