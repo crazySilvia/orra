3133ad9008084cb4255464127de26076b7bb436e
crazySilvia <71493992+crazySilvia@users.noreply.github.com>
2022-02-09 07:41:28 +0100

    + FE Anzeige der Artikel ermöglicht, mit den zugehörigen Buttons (#11)

--- frontend/src/Components/ArtikelComponent.tsx
+++ frontend/src/Components/ArtikelComponent.tsx
@@ -1,9 +1,22 @@
-import {Artikel} from "../Model/Artikel";
+import "./ArtikelComponent.css"
+import React from "react";
+import {IArtikel} from "../Model/Artikel";
 
-export default function ArtikelComponent({artikel}:{artikel:Artikel}){
+
+export default function ArtikelComponent({artikel}:{artikel:IArtikel}){
     return(
-        <div>
-            {artikel.name}
+        <div className="component">
+            <div className="teil">
+                <button>{artikel.anzahl}</button>
+            </div>
+            <div className="text">
+                {artikel.name}
+            </div>
+            <div className="teil">
+                <button >+</button>
+                <button >-</button>
+                <button >x</button>
+            </div>
         </div>
     )
 
--- frontend/src/Model/Artikel.ts
+++
@@ -1,4 +0,0 @@
-export interface Artikel{
-    name: string
-    anzahle: number
-}
